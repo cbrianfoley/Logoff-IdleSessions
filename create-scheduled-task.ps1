a0c66adec3c2c63f0656121e54e200f69e0cc7bb
cbrianfoley <cbrianfoley@users.noreply.github.com>
2019-12-12 08:06:42 -0600

    Update create-scheduled-task.ps1

--- create-scheduled-task.ps1
+++ create-scheduled-task.ps1
@@ -4,7 +4,7 @@
 robocopy . C:\Windows\System32\WindowsPowerShell\v1.0\Scripts\ LogOff-IdleSessions.ps1
 
 #creates a scheduled task that will run at logon and repeat every hour
-$Script = "C:\Windows\System32\WindowsPowerShell\v1.0\Scripts\log-off-idle-sessions.ps1"
+$Script = "C:\Windows\System32\WindowsPowerShell\v1.0\Scripts\LogOff-IdleSessions.ps1"
 Unregister-ScheduledTask -TaskName 'log-off-idle-sessions' -Confirm:$false
 $Action = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"-Argument "-File $($Script)"
 $Trigger = New-ScheduledTaskTrigger -AtLogOn
